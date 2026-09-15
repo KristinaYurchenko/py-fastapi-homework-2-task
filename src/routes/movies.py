a57f83b3c923d8b7bf38cc274237bc8c5d78e0d9
Kristina <urchenko.kristina@gmail.com>
2026-09-15 19:27:44 +0200

    Added crud operations for movie

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,13 +1,178 @@
+import math
+
 from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select, func
+from fastapi.encoders import jsonable_encoder
+from fastapi.openapi.utils import status_code_ranges
+from sqlalchemy import select, func, update
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import selectinload, joinedload
 
-from database import get_db, MovieModel
-from database.models import CountryModel, GenreModel, ActorModel, LanguageModel
-
+from database import get_db, MovieModel, models
+from database.models import GenreModel, ActorModel, LanguageModel, CountryModel
+from schemas import MovieListResponseSchema, MovieDetailSchema, MovieCreateSchema
+from schemas.movies import MovieUpdateSchema
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies/", response_model=MovieListResponseSchema)
+async def get_paginated_movie(page: int = Query(1, ge=1),
+                              per_page: int = Query(10, ge=1, le=20),
+                              db: AsyncSession = Depends(get_db)):
+
+    offset = (per_page * page) - per_page
+    total_items =  await db.scalar(select(func.count()).select_from(MovieModel))
+    total_pages = math.ceil(total_items / per_page)
+    result = await db.scalars(select(MovieModel).order_by(MovieModel.id.desc()).offset(offset).limit(per_page))
+    movies = result.all()
+    if not movies:
+        raise HTTPException(detail="No movies found.", status_code=404)
+    prev_page = f"/theater/movies/?page={page-1}&per_page={per_page}" if page > 1 else None
+    next_page = f"/theater/movies/?page={page+1}&per_page={per_page}" if page < total_pages else None
+    return MovieListResponseSchema(movies=movies,
+    prev_page=prev_page,
+    next_page=next_page,
+    total_pages=total_pages,
+    total_items=total_items,)
+
+
+
+@router.post("/movies/", response_model=MovieDetailSchema, status_code=201)
+async def create_movie(movie_data: MovieCreateSchema, db: AsyncSession = Depends(get_db)):
+    genres = []
+    for genre_name in movie_data.genres:
+        genre = await db.scalar(select(GenreModel).where(GenreModel.name == genre_name))
+        if not genre:
+            genre = GenreModel(name=genre_name)
+            db.add(genre)
+        genres.append(genre)
+    actors = []
+    for actor_name in movie_data.actors:
+        actor = await db.scalar(select(ActorModel).where(ActorModel.name== actor_name))
+        if not actor:
+            actor = ActorModel(name=actor_name)
+            db.add(actor)
+        actors.append(actor)
+
+    languages = []
+    for language_name in movie_data.languages:
+        language = await db.scalar(select(LanguageModel).where(LanguageModel.name==language_name))
+        if not language:
+            language = LanguageModel(name=language_name)
+            db.add(language)
+        languages.append(language)
+
+
+    country = await db.scalar(select(CountryModel).where(CountryModel.code==movie_data.country))
+    if not country:
+        country = CountryModel(code=movie_data.country)
+        db.add(country)
+
+
+    existing_movie = await db.scalar(
+        select(MovieModel).where(
+            MovieModel.name == movie_data.name,
+            MovieModel.date == movie_data.date,
+        )
+    )
+    if existing_movie:
+        raise HTTPException(detail=f"A movie with the name '{movie_data.name}' and release date '{movie_data.date}' already exists.", status_code=409)
+
+    movie = MovieModel(
+        name=movie_data.name,
+        date=movie_data.date,
+        score=movie_data.score,
+        overview=movie_data.overview,
+        status=movie_data.status,
+        budget=movie_data.budget,
+        revenue=movie_data.revenue,
+        country=country,  # об'єкт CountryModel — SQLAlchemy сам підставить country_id
+        genres=genres,
+        actors=actors,
+        languages=languages,
+    )
+
+    db.add(movie)
+    try:
+        await db.commit()
+    except IntegrityError:
+        await db.rollback()
+        raise HTTPException(status_code=400, detail="Invalid input data.")
+    movie = await db.scalar(select(MovieModel).options(
+        selectinload(MovieModel.genres),
+        selectinload(MovieModel.actors),
+        selectinload(MovieModel.languages),
+        joinedload(MovieModel.country),
+    ).where(MovieModel.id == movie.id))
+
+    return movie
+
+
+@router.get("/movies/{movie_id}/", response_model=MovieDetailSchema)
+async def retrieve_movie(movie_id: int, db: AsyncSession = Depends(get_db)):
+    # movie = await db.scalar(select(MovieModel).where(models.MovieModel.id == movie_id))
+    movie = await db.scalar(select(MovieModel).options(
+        selectinload(MovieModel.genres),
+        selectinload(MovieModel.actors),
+        selectinload(MovieModel.languages),
+        joinedload(MovieModel.country),
+    ).where(MovieModel.id==movie_id))
+
+    if movie is None:
+        raise HTTPException(status_code=404, detail="Movie with the given ID was not found.")
+    return movie
+
+
+@router.delete("/movies/{movie_id}/", status_code=204)
+async def delete_movie(movie_id: int, db: AsyncSession = Depends(get_db)):
+    movie = await db.scalar(select(MovieModel).where(MovieModel.id==movie_id))
+    if movie is None:
+        raise HTTPException(status_code=404, detail="Movie with the given ID was not found.")
+    await db.delete(movie)
+    await db.commit()
+
+
+
+
+
+@router.patch("/movies/{movie_id}/", status_code=200)
+async def update_movie(movie_id: int, movie_data: MovieUpdateSchema, db: AsyncSession = Depends(get_db)):
+    db_movie = await db.scalar(select(MovieModel).where(MovieModel.id==movie_id))
+    if db_movie is None:
+        raise HTTPException(status_code=404, detail="Movie with the given ID was not found.")
+
+    values = movie_data.model_dump(exclude_unset=True)
+    if not values:
+        return db_movie
+
+    await db.execute(update(models.MovieModel).where(models.MovieModel.id == movie_id).values(**values))
+    try:
+        await db.commit()
+    except IntegrityError:
+        await db.rollback()
+        raise HTTPException(status_code=400, detail="Invalid input data.")
+    await db.refresh(db_movie)
+    return {"detail": "Movie updated successfully."}
+
+
+
+
+
+
+
+# def update_author(db: Session, author_id: int, author_data: schemas.AuthorUpdate):
+#     db_author = get_author(db=db, author_id=author_id)
+#     if db_author is None:
+#         return None
+#     values = author_data.model_dump(exclude_unset=True)
+#     if not values:
+#         return db_author
+#     db.execute(
+#         update(models.DBAuthor)
+#         .where(models.DBAuthor.id == author_id)
+#         .values(**values)
+#     )
+#     db.commit()
+#     db.refresh(db_author)
+#     return db_author
--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,102 @@
-# Write your code here
+import datetime
+
+from dateutil.relativedelta import relativedelta
+from pydantic import BaseModel, ConfigDict, Field, field_validator
+
+from database.models import MovieStatusEnum
+
+
+class GenreSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str
+
+
+class ActorSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str
+
+
+class LanguageSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str
+
+
+class CountrySchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    code: str
+    name: str | None
+
+
+class MovieListItemSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str
+    date: datetime.date
+    score: float
+    overview: str
+
+
+class MovieDetailSchema(MovieListItemSchema):
+    status: MovieStatusEnum
+    budget: float
+    revenue: float
+    country: CountrySchema
+    genres: list[GenreSchema]
+    actors: list[ActorSchema]
+    languages: list[LanguageSchema]
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: list[MovieListItemSchema]
+    prev_page: str | None
+    next_page: str | None
+    total_pages: int
+    total_items: int
+
+
+class MovieCreateSchema(BaseModel):
+    name: str = Field(max_length=255)
+    date: datetime.date
+    score: float = Field(ge=0, le=100)
+    overview: str
+    status: MovieStatusEnum
+    budget: float = Field(ge=0)
+    revenue: float = Field(ge=0)
+    country: str = Field(min_length=2, max_length=3)
+    genres: list[str]
+    actors: list[str]
+    languages: list[str]
+
+    @field_validator("date")
+    @classmethod
+    def validate_date_not_too_far_in_future(cls, value: datetime.date) -> datetime.date:
+        max_date = datetime.date.today() + relativedelta(years=1)
+        if value > max_date:
+            raise ValueError("The date must not be more than one year in the future.")
+        return value
+
+class MovieUpdateSchema(BaseModel):
+    name: str | None = Field(default=None, max_length=255)
+    date: datetime.date | None = None
+    score: float | None = Field(default=None, ge=0, le=100)
+    overview: str | None = None
+    status: MovieStatusEnum | None = None
+    budget: float | None = Field(default=None, ge=0)
+    revenue: float | None = Field(default=None, ge=0)
+
+    @field_validator("date")
+    @classmethod
+    def validate_date_not_too_far_in_future(cls, value: datetime.date) -> datetime.date:
+        max_date = datetime.date.today() + relativedelta(years=1)
+        if value > max_date:
+            raise ValueError("The date must not be more than one year in the future.")
+        return value
